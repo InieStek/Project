007358625b77a1242b0b005a2a48e46f70368405
Michal Paukszto <michal.paukszto@gmail.com>
2022-01-18 15:51:23 +0100

    small remark

--- src/main/java/com/sda/frontend/UserInterface.java
+++ src/main/java/com/sda/frontend/UserInterface.java
@@ -15,7 +15,7 @@ public class UserInterface {
         while (true) {
             System.out.println("Witaj w aplikacji WeatherApp!, co chcesz zrobić?");
             System.out.println("1. Dodanie konkretnej lokalizacji");
-            System.out.println("2. Wyszukaj pogodę");
+            System.out.println("2. Wyszukaj pogodę"); // todo lokalizację
             System.out.println("3. Pobierz wartość pogody");
             System.out.println("0. Wyjdź z aplikacji");
 
@@ -23,13 +23,10 @@ public class UserInterface {
 
             switch (option) {
                 case 1:
-
                     break;
                 case 2:
-
                     break;
                 case 3:
-
                     break;
                 case 0:
                     System.out.println("Do zobaczenia!");
