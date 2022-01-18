411ad284e2ab4c40e34451b671d27ab9bdeef96e
Michal Paukszto <michal.paukszto@gmail.com>
2022-01-18 16:01:20 +0100

    review

--- src/main/java/com/sda/weather/WeatherController.java
+++ src/main/java/com/sda/weather/WeatherController.java
@@ -1,18 +1,18 @@
-package com.sda.weather;
+package com.sda.weather; // todo com.sda.weather.localization
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class WeatherController {
+public class WeatherController { // todo LocalizationController, LocalizationService, LocalizationRepository
 
     private final WeatherService weatherService;
     private final ObjectMapper objectMapper;
 
     public String createLocalization(String data) {
         try {
-            Localization newLocalization = objectMapper.readValue(data, Localization.class);
+            Localization newLocalization = objectMapper.readValue(data, Localization.class); // todo create and use a LocalizationDTO class
             Localization localization = weatherService.createLocalization(newLocalization.getCity(), newLocalization.getCountry(), newLocalization.getRegion(), newLocalization.getLatitude(), newLocalization.getLongitude());
             return objectMapper.writeValueAsString(localization);
         } catch (IllegalArgumentException e) {
--- src/main/java/com/sda/weather/WeatherService.java
+++ src/main/java/com/sda/weather/WeatherService.java
@@ -6,18 +6,18 @@ import lombok.RequiredArgsConstructor;
 public class WeatherService {
     private final WeatherRepository weatherRepository;
 
+    // todo write some tests
     Localization createLocalization(String city, String country, String region, Float latitude, Float longitude) {
         if (city == null || country == null|| latitude == null || longitude == null || city.isBlank() || country.isBlank() || latitude > 90.0 || latitude < -90.0 || longitude > 180.0 || longitude < -180.0) {
-            throw new IllegalArgumentException("Walidacja danych nie powiodła się");
+            throw new IllegalArgumentException("Walidacja danych nie powiodła się"); // todo specify this message, what went wong?
         }
         Localization localization = new Localization();
         localization.setCity(city);
         localization.setCountry(country);
-        localization.setRegion(region);
+        localization.setRegion(region); // todo if it's eg. "   " it will be stored in the database, we want to avoid this
         localization.setLatitude(latitude);
         localization.setLongitude(longitude);
 
-        Localization localizationSave = weatherRepository.save(localization);
-        return localizationSave;
+        return weatherRepository.save(localization);
     }
 }
